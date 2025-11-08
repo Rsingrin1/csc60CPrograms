642500b1f823e3443b528380af3a332bce9cd324
Rsingrin1 <rsingrin@csus.edu>
2025-11-07 17:07:50 -0800

    finished the structure assigment

--- structureAssignment/main.c
+++ structureAssignment/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 typedef struct {
     char ssn[12];
@@ -12,6 +12,19 @@ typedef struct
     int gradeArray[3];
     char* nameOfSchool;
 } Student;
+
+void display(Student s[2])
+{
+    Student* ptr = s;
+
+    for (int i = 0; i < 2; i++) {
+        printf("SSN: %s\n", (ptr + i)->person.ssn);
+        printf("Last Name: %s\n", (ptr + i)->person.lname);
+        printf("Grades: %d, %d, %d\n", (ptr + i)->gradeArray[0], (ptr + i)->gradeArray[1], (ptr + i)->gradeArray[2]);
+        printf("School: %s\n", (ptr + i)->nameOfSchool);
+        printf("\n");
+    }
+}
 
 
 
@@ -29,15 +42,15 @@ int main()
         for (int j = 0; j < 3; j++) {
             scanf("%d", &students[i].gradeArray[j]);
         }
-        students[i].nameOfSchool = "Default School"; // Assigning a default school name
+        char* schoolName = (char*)malloc(40 * sizeof(char));
+        printf("Enter name of school for student %d: ", i + 1);
+        scanf("%s", schoolName);
+        students[i].nameOfSchool = schoolName;
     }
+    display(students);
 
     for (int i = 0; i < 2; i++) {
-        printf("\nStudent %d Information:\n", i + 1);
-        printf("SSN: %s\n", students[i].person.ssn);
-        printf("Last Name: %s\n", students[i].person.lname);
-        printf("Grades: %d, %d, %d\n", students[i].gradeArray[0], students[i].gradeArray[1], students[i].gradeArray[2]);
-        printf("School: %s\n", students[i].nameOfSchool);
+        free(students[i].nameOfSchool);
     }
 
     return 0;
